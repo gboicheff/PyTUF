9dfb76109e6137c64e40ff9e612d9aa08aa5b397
Grant Boicheff <gboicheff@gmail.com>
2022-01-26 17:54:23 -0500

    more components

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import eel
 
 # Set web files folder
 eel.init('web')
-
+ 
 # @eel.expose                         # Expose this function to Javascript
 # def say_hello_py(x):
 #     print('Hello from %s' % x)

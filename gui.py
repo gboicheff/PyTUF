75393b3e4725b7450d53db6f4725256292029dad
maxkrieger2000 <maxkrieger@ufl.edu>
2022-03-18 20:32:21 -0400

    UI changes

--- gui.py
+++ gui.py
@@ -1,5 +1,8 @@
 import eel
 import os
+from tkinter import filedialog
+
+from paths import PathDict, PathType
 
 # Set web files folder
 eel.init('web')
@@ -12,11 +15,18 @@ eel.init('web')
 # eel.say_hello_js('Python World!')   # Call a Javascript function
 
 @eel.expose
-def get_folder():
-    path = os.getcwd() + "/collections"
-    filelist = os.listdir(path)
+def get_folder(foldname):
+    if (foldname == "/collections"):
+        foldname = os.getcwd() + "/collections"
+    filelist = os.listdir(foldname)
     print(filelist)
     return filelist
+
+@eel.expose
+def select_path():
+    foldername = filedialog.askdirectory()
+    return foldername
+
 
 
 eel.start('hello.html', size=(1200, 1000), mode='chrome')  # Start
--- web/js/demojs.js
+++ web/js/demojs.js
@@ -1,23 +1,73 @@
 
 
-
+/*
 function htmlload() {
     alert("testing on load")
+    if(sessionStorage.getItem('curColPath') === null)
+    {
+        sessionStorage.setItem('curColPath', '/collections')
+    }
+
     
   }
+*/
+async function setpath(n) {
+    //call python to use tk:
+    newcolfold = await eel.select_path()()
+
+    if(n == 1)
+    {
+        sessionStorage.setItem('curColPath', newcolfold)
+    }
+    else if(n == 2)
+    {
+        sessionStorage.setItem('curExtractPath', newcolfold)
+    }
+    else if(n == 3)
+    {
+        sessionStorage.setItem('curScoringPath', newcolfold)
+    }
+
+}
 
 
 document.addEventListener("DOMContentLoaded", async function() {
     
 
-    folderelems = await eel.get_folder()()
+    if(sessionStorage.getItem('curColPath') === null)
+    {
+        sessionStorage.setItem('curColPath', '/collections')
+    }
+    if(sessionStorage.getItem('curExtractPath') === null)
+    {
+        sessionStorage.setItem('curExtractPath', '/collections')
+    }
+    if(sessionStorage.getItem('curScoringPath') === null)
+    {
+        sessionStorage.setItem('curScoringPath', '/collections')
+    }
 
-    
+    folderelems = await eel.get_folder(sessionStorage.getItem('curColPath'))()
     folderelems.forEach(elem => {
-        alert(elem)
         var option = document.createElement("option")
         option.text = elem
         document.getElementById("data-select").add(option)
+    });
+
+
+    folderelems = await eel.get_folder(sessionStorage.getItem('curExtractPath'))()
+    folderelems.forEach(elem => {
+        var option = document.createElement("option")
+        option.text = elem
+        document.getElementById("feature-extract-select").add(option)
+    });
+
+
+    folderelems = await eel.get_folder(sessionStorage.getItem('curScoringPath'))()
+    folderelems.forEach(elem => {
+        var option = document.createElement("option")
+        option.text = elem
+        document.getElementById("scoring-exclusion-select").add(option)
     });
 
 
